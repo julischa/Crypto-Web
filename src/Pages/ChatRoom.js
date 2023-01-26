6c22d0d44d7200d3bfe9023921268ac41825d8b5
julescwrt <julianeschawert@gmail.com>
2023-01-26 20:36:57 +0100

    Inputfield for Name

--- src/Pages/ChatRoom.js
+++ src/Pages/ChatRoom.js
@@ -3,27 +3,38 @@ import React, { useState } from "react";
 const ChatRoom = (props) => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
+  const [userName, setUserName] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMessages([...messages, newMessage]);
+    setMessages([...messages, userName + ': ' + newMessage]);
     setNewMessage("");
   };
 
   return (
     <div>
-      <h2 className='text-center'>My {props.coin.name} prediction:</h2>
+      <h2 className='text-center'>My prediction for {props.coin.name}:</h2>
+      <br></br>
       <form className='text-center' onSubmit={handleSubmit}>
+        <input className="input-chat text-center" type="text" placeholder="my Nickname" value={userName} onChange={(e) => setUserName(e.target.value)} />
+        <br></br>
+        <br></br>
         <input
+          className="input-chat text-center"
           type="text"
+          placeholder="my prediction"
           value={newMessage}
           onChange={(e) => setNewMessage(e.target.value)}
         />
-        <button className='text-center' type="submit">Comment</button>
+        <button className='inputbutton' type="submit">Comment</button>
+        <br></br><br></br>
         <div>
-        {messages.map((message, index) => (
-          <h3 className='text-center' key={index}>{message}</h3>
-        ))}
+        {messages.map((message, index) => {
+            const [userName, messageContent] = message.split(':')
+            return (
+              <h3 className='text-center' key={index}>{userName}: {messageContent}</h3>
+            )
+          })}
       </div>
       </form>
     </div>
